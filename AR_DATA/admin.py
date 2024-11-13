ba38c1646d917912876a3ee90c3becbb56c87ad7
minju123486 <minju123486@gmail.com>
2024-11-14 06:42:44 +0900

    Test

--- AR_DATA/admin.py
+++ AR_DATA/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 
-from .models import User_data
-# Register your models here.
+from .models import User_data, Tour_place, Check, StampTable,  history
+# Register your models here. 
 admin.site.register(User_data)
+admin.site.register(Tour_place)
+admin.site.register(Check)
+admin.site.register(StampTable)
+admin.site.register(history)
